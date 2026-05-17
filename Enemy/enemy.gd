0ab1617307a240c1250973419860dd75ca4f153c
MrMaRc0s <xristosssmarkosss@gmail.com>
2026-05-18 01:12:42 +0300

    Added Health system

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,6 +1,10 @@
 extends CharacterBody3D
 
+@export var max_health : float = 20
+
 @onready var rig: Node3D = $Rig
+@onready var health_component: HealthComponent = $HealthComponent
 
 func _ready() -> void:
 	rig.set_active_mesh(rig.villager_meshes.pick_random())
+	health_component.update_max_health(max_health)
