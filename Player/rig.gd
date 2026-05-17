4daef42396afe42f0110cf0d361218b0e839de4e
MrMaRc0s <xristosssmarkosss@gmail.com>
2026-05-17 20:53:32 +0300

    make the transition from idle to run smoother

--- Player/rig.gd
+++ Player/rig.gd
@@ -1,11 +1,17 @@
 extends Node3D
 
-@onready var animation_tree: AnimationTree = $AnimationTree
+@export var animations_speed : float = 10
+
+@onready var animation_tree : AnimationTree = $AnimationTree
 
 var run_path : String = "parameters/MoveSpace/blend_position"
+var run_weight_target : float= -1
+
+func _physics_process(delta: float) -> void:
+	animation_tree[run_path] = move_toward(animation_tree[run_path], run_weight_target, delta * animations_speed)
 
 func update_animation_tree(direction: Vector3) -> void:
 	if direction.is_zero_approx():
-		animation_tree[run_path] = -1.0
+		run_weight_target = -1.0
 	else:
-		animation_tree[run_path] = 1.0
+		run_weight_target = 1.0
