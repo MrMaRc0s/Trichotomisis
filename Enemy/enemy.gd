cfa562e2ac0c7d7e96cb9eb1ed7ee134e5490df9
Christos Markos <xristosssmarkosss@gmail.com>
2026-05-24 04:37:51 +0300

    Navigation Agent

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -9,6 +9,8 @@ class_name Enemy
 @onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
 @onready var player_detector: ShapeCast3D = $Rig/PlayerDetector
 @onready var area_attack: ShapeCast3D = $Rig/AreaAttack
+@onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
+
 
 @onready var player : Player = get_tree().get_first_node_in_group("Player")
 
@@ -17,6 +19,10 @@ func _ready() -> void:
 	health_component.update_max_health(max_health)
 	
 func _physics_process(delta: float) -> void:
+	var velocity_target := Vector3.ZERO
+	navigation_agent_3d.target_position = player.global_position
+	orient_rig(navigation_agent_3d.get_next_path_position())
+	
 	if rig.is_idle():
 		check_for_attacks()
 		
@@ -36,3 +42,10 @@ func _on_health_component_defeat() -> void:
 
 func _on_rig_heavy_attack() -> void:
 	area_attack.deal_damage(10)
+	
+func orient_rig(target_position: Vector3) -> void:
+	target_position.y = rig.global_position.y
+	if rig.global_position.is_equal_approx(target_position):
+		return
+	
+	rig.look_at(target_position, Vector3.UP, true)
