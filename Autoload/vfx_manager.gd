74fe90d3d504261ef8c9ae8b62f54aa697ef60f3
Christos Markos <xristosssmarkosss@gmail.com>
2026-05-24 04:25:16 +0300

    Navigation Region

--- Autoload/vfx_manager.gd
+++ Autoload/vfx_manager.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 const DAMAGE_NUMBER = preload("uid://bljtqqtanrofb")
 
-func spawn_damage_number(damage: int, color: Color, position: Vector3) -> void:
+func spawn_damage_number(damage: int, color: Color, position_in: Vector3) -> void:
 	var new_number = DAMAGE_NUMBER.instantiate()
-	new_number.setup(damage, color, position)
+	new_number.setup(damage, color, position_in)
 	add_child(new_number)
